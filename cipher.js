494458cf8027f65d375888243b95a62cd86470ef
Kauana Maria Agostini <kauanaagostini@gmail.com>
2020-10-30 15:51:50 -0300

    primeiro envido de arquivos ao git, com pagina concluida, css concluido, e os js devidamente separados

--- cipher.js
+++ cipher.js
@@ -1,5 +1,47 @@
+//construção dos parametros para o calculo da cifra de cesar,
+//variavel para trazer a frase final
+const codi1aLetra = 65
+const tamAlfabeto = 26
+let fraseFinalcrip = ""
+let fraseFinaldescrip = ""
+
 const cipher = {
-  // ...
+//funcao de criptografar, e buscar as informações do Index
+//retorando a frase final letra a letra incrementada.
+  encode (numbercrip, msgcrip) {
+    for (let i=0; i<=msgcrip.length; i++){
+      let resultado1 = (msgcrip.charCodeAt(i) + numbercrip - codi1aLetra) % tamAlfabeto
+      let resultado2 = resultado1 + codi1aLetra
+      let imprimir = String.fromCharCode(resultado2)
+      fraseFinalcrip += imprimir
+    }
+    return fraseFinalcrip
+  },
+//funcao de descriptografar, e buscar as informações do Index.
+//retorando a frase final letra a letra incrementada, trabalhando com os bugs de numeros negativos e numero que passam de um alfabeto.
+  decode (numberdescrip, msgdescrip){
+    for (let i=0; i<=msgdescrip.length; i++){
+      let resultado1 = (msgdescrip.charCodeAt(i) - numberdescrip - codi1aLetra)
+      let negativo = (-resultado1 + tamAlfabeto)
+      if(resultado1 <0 & negativo <=1){
+          let resultado2 = resultado1 + tamAlfabeto
+          let resultado3 = resultado2 + codi1aLetra
+          let imprimir = String.fromCharCode(resultado3)
+          fraseFinaldescrip += imprimir        
+      } else if (resultado1 <0 & negativo >1){
+               let resultado2 = (resultado1 + (tamAlfabeto * Math.trunc(negativo)) + tamAlfabeto) % tamAlfabeto
+               let resultado3 = resultado2 + codi1aLetra
+               let imprimir = String.fromCharCode(resultado3)
+               fraseFinaldescrip += imprimir
+          } else {
+                let resultado2 = resultado1 % tamAlfabeto
+                let resultado3 = resultado2 + codi1aLetra
+                let imprimir = String.fromCharCode(resultado3)
+                fraseFinaldescrip += imprimir
+              }
+        }
+        return fraseFinaldescrip
+  }
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
+//variavel para trazer a frase final
+//variavel para informar que o botao existe
+let fraseFinalcrip = ""
+const crip = document.getElementById("crip")
+let fraseFinaldescrip = ""
+const descrip = document.getElementById("descrip")
 
-console.log(cipher);
+//função buscar dados do HTML enviar para Cipher.encode e devolver na tela.
+function dadoscripto(event){
+    event.preventDefault()
+    let msgcrip = document.getElementById("msgcrip").value
+    let numbercrip = parseInt(document.getElementById("numbercrip").value)
+    fraseFinalcrip = cipher.encode(numbercrip,msgcrip)
+    document.getElementById("respcrip").innerHTML=fraseFinalcrip
+}
+//informando ao JS o que ele deve fazer quando o botao cripto for precionado.
+crip.addEventListener("click", dadoscripto)
+
+//função buscar dados do HTML enviar para Cipher.decode e devolver na tela.
+function dadosdescripto(event){
+    event.preventDefault()
+    let msgdescrip = document.getElementById("msgdescrip").value
+    let numberdescrip = parseInt(document.getElementById("numberdescrip").value)
+    fraseFinaldescrip = cipher.decode(numberdescrip, msgdescrip)
+    document.getElementById("respdescrip").innerHTML=fraseFinaldescrip
+}
+//informando ao JS o que ele deve fazer quando o botao descripto for precionado.
+descrip.addEventListener("click", dadosdescripto)
